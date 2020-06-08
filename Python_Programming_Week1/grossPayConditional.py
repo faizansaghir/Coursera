5b5fc9bcafc209dcc5d7f08616e95b45645ddca6
faizansaghir <49108070+faizansaghir@users.noreply.github.com>
2020-06-09 00:10:47 +0530

    Improved grossPayConditional code and included function for calculating grossPay

--- Python_Programming_Week1/grossPayConditional.py
+++ Python_Programming_Week1/grossPayConditional.py
@@ -1,9 +1,11 @@
-hrs = input("Enter Hours:")
-h = float(hrs)
-rate=input("Enter Rate:")
-r=float(rate)
-if(h>40):
-    pay=40*r+1.5*(h-40)*r
-else:
-    pay=h*r
-print(pay)
+def calculatePay(h,r):
+    if(h<=40):
+        result=h*r
+    else:
+        result=40*r+1.5*(h-40)*r
+    return result
+
+hrs =float(input("Enter Hours:"))
+rate=float(input("Enter Rate:"))
+pay=calculatePay(hrs,rate)
+print('Pay',pay)
